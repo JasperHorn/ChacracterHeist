62a37d3c2941886ede6259f4a28a49354bb3d78b
Jasper Horn <jasperhorn@gmail.com>
2019-05-01 19:49:00 +0200

    [#2] Reuse the same sentence

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 
 import readchar
+import colorama
+import os
+
+def printPartial(text):
+    print(text, end = '', flush = True)
+
+def clearScreen():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def setCursorPosition(x, y):
+    printPartial('\x1b[%d;%dH' % (x, y))
+
+clearScreen()
+colorama.init()
+
+printPartial("Your character: ")
 
 while True:
-    bla = readchar.readkey()
-    print("Your character: " + bla)
+    input = readchar.readkey()
+    setCursorPosition(1, 17)
+
+    if len(input) == 1:
+        printPartial(input)
+    else:
+        # Print as a list, so it doesn't get interpreted as ANSI code...
+        printPartial(list(input))
