18467b0873d561282dca4f411dd678730fabfcc4
Jasper Horn <jasperhorn@gmail.com>
2019-05-04 12:57:17 +0200

    [#16] Add door instructions to intro screen

--- GUI/introScreen.py
+++ GUI/introScreen.py
@@ -9,5 +9,6 @@ def drawIntroScreen():
     consoleUtils.specialPrint(10, 4, '- This is you: ' + Fore.CYAN + '@' + Style.RESET_ALL)
     consoleUtils.specialPrint(12, 4, '- You have to acquire the treasure: ' + Fore.GREEN + Style.BRIGHT + '*' + Style.RESET_ALL)
     consoleUtils.specialPrint(14, 4, '- Next you have to escape: ' + Fore.GREEN + Style.BRIGHT + 'O' + Style.RESET_ALL)
-    consoleUtils.specialPrint(16, 4, '- You should collect money on your way: ' + Fore.YELLOW + Style.BRIGHT + '$' + Style.RESET_ALL)
-    consoleUtils.specialPrint(20, 20, "Press any button to continue")
+    consoleUtils.specialPrint(16, 4, '- You may have to move through doors: ' + Fore.GREEN + 'Z' + Style.RESET_ALL)
+    consoleUtils.specialPrint(18, 4, '- You should collect money on your way: ' + Fore.YELLOW + Style.BRIGHT + '$' + Style.RESET_ALL)
+    consoleUtils.specialPrint(22, 20, "Press any button to continue")
