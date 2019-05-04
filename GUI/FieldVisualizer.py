1a98ce507053696eab20ae33dda71cb6d39e40a4
Jasper Horn <jasperhorn@gmail.com>
2019-05-04 12:49:35 +0200

    [#16] Extract drawing of object into method
    This so it can be also be used elsewhere.

--- GUI/FieldVisualizer.py
+++ GUI/FieldVisualizer.py
@@ -16,19 +16,7 @@ class FieldVisualizer(CharacterObserver):
 
         for x in range(0, field.width):
             for y in range(0, field.height):
-                object = field.getObjectAtLocation(x, y)
-                if object is None:
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, ' ', Back.BLACK)
-                elif isinstance(object, Wall):
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'X', Fore.WHITE + Back.BLUE)
-                elif isinstance(object, Money):
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, '$', Fore.YELLOW + Style.BRIGHT + Back.BLACK)
-                elif isinstance(object, TargetTreasure):
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, '*', Fore.GREEN + Style.BRIGHT + Back.BLACK)
-                elif isinstance(object, Exit):
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'O', Fore.CYAN + Back.BLUE)
-                elif isinstance(object, Door):
-                    consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'Z', Fore.GREEN + Back.BLUE)
+                self.drawObjectAtLocation(x, y)
 
         consoleUtils.specialPrint(FieldVisualizer.offset[1] + character.y, FieldVisualizer.offset[0] + character.x, '@', Fore.CYAN + Back.BLACK)
 
@@ -41,3 +29,18 @@ class FieldVisualizer(CharacterObserver):
             for y in range(0, self.field.height):
                 if isinstance(self.field.getObjectAtLocation(x, y), Exit):
                     consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'O', Fore.GREEN + Style.BRIGHT + Back.BLACK)
+
+    def drawObjectAtLocation(self, x, y):
+        object = self.field.getObjectAtLocation(x, y)
+        if object is None:
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, ' ', Back.BLACK)
+        elif isinstance(object, Wall):
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'X', Fore.WHITE + Back.BLUE)
+        elif isinstance(object, Money):
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, '$', Fore.YELLOW + Style.BRIGHT + Back.BLACK)
+        elif isinstance(object, TargetTreasure):
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, '*', Fore.GREEN + Style.BRIGHT + Back.BLACK)
+        elif isinstance(object, Exit):
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'O', Fore.CYAN + Back.BLUE)
+        elif isinstance(object, Door):
+            consoleUtils.specialPrint(FieldVisualizer.offset[1] + y, FieldVisualizer.offset[0] + x, 'Z', Fore.GREEN + Back.BLUE)
