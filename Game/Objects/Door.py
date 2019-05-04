cfae1149af00cf2264630446274eb1c38d1cc815
Jasper Horn <jasperhorn@gmail.com>
2019-05-04 16:32:28 +0200

    [#9] Remove fog of war upon entering door

--- Game/Objects/Door.py
+++ Game/Objects/Door.py
@@ -2,4 +2,7 @@
 from .Object import Object
 
 class Door(Object):
-    pass
+    def stepOn(self, character):
+        for dx in [-1, 1]:
+            for dy in [-1, 1]:
+                self.field.spreadViewingFromPoint(character.x + dx, character.y + dy)
