a3766d2ab55a2f219061204b7936d9de5cae9c10
Jasper Horn <jasperhorn@gmail.com>
2019-05-18 18:56:09 +0200

    [#10] Make sure enemy doesn't make FoW disappear

--- Game/Objects/Door.py
+++ Game/Objects/Door.py
@@ -3,6 +3,7 @@ from .Object import Object
 
 class Door(Object):
     def stepOn(self, character):
-        for dx in [-1, 1]:
-            for dy in [-1, 1]:
-                self.field.spreadViewingFromPoint(character.x + dx, character.y + dy)
+        if character.isPlayer():
+            for dx in [-1, 1]:
+                for dy in [-1, 1]:
+                    self.field.spreadViewingFromPoint(character.x + dx, character.y + dy)
