2ce4384a582fe3e5d80d6e6d388ae638e0f00538
Jasper Horn <jasperhorn@gmail.com>
2019-05-19 00:08:05 +0200

    [#10] Properly fix guards near exit this time
    This wasn't properly testeed because of the random nature of the event
    (for testing this time, the code was temporarily modified not to be
    random anymore).

--- Game/Objects/Exit.py
+++ Game/Objects/Exit.py
@@ -3,7 +3,7 @@ from .Object import Object
 
 class Exit(Object):
     def isPassable(self, character):
-        return character.isPlayer and character.hasTarget
+        return character.isPlayer() and character.hasTarget
 
     def stepOn(self, player):
         player.exited = True
