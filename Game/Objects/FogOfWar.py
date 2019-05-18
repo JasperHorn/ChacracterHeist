73e371074d9ad67b3928da9041f111ace152f8e2
Jasper Horn <jasperhorn@gmail.com>
2019-05-18 18:59:56 +0200

    [#10] Enemy should be hidden by Fog of war
    This also prevents the previous issue where the Fog of War under the
    player was kept until the player stepped on a door.

--- Game/Objects/FogOfWar.py
+++ Game/Objects/FogOfWar.py
@@ -3,7 +3,7 @@ from .Object import Object
 
 class FogOfWar(Object):
     def getVisibilityPriority(self):
-        return 1000
+        return 1000000
 
     def view(self):
         self.field.removeObject(self)
