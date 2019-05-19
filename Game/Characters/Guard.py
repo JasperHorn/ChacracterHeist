eb0b37d78b4454dfb08ffec3a64d100685d00d6a
Jasper Horn <jasperhorn@gmail.com>
2019-05-19 02:23:25 +0200

    [#10] Make Guard find wall to follow

--- Game/Characters/Guard.py
+++ Game/Characters/Guard.py
@@ -34,7 +34,30 @@ class Guard(Character):
         self.actUp()
 
     def findWallToFollow(self):
-        pass
+        for (x, y) in self.searchArea(2):
+            x += self.x
+            y += self.y
+
+            if self.field.getVisibleObjectAtLocation(x, y) is not None:
+                self.followingWall = (x, y)
+
+    def searchArea(self, radius):
+        for distance in range(1, radius + 1):
+            for coordinate in self.manhattanCirle(distance):
+                yield coordinate
+
+    def manhattanCirle(self, radius):
+        for n in range(0, radius * 4):
+            # Start (0, -r) instead of (r, 0)
+            n = (n - radius) % (radius * 4)
+
+            if n <= radius * 2:
+                x = radius - n
+                y = radius - abs(x)
+            else:
+                x = -3 * radius + n
+                y = -radius + abs(x)
+            yield (x, y)
 
     def moveAlongWall(self):
         pass
